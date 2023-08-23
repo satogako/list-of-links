15db950abe3dea16419c3c124f8e3e72c9ccf728
Satogako <genevaq@ukr.net>
2023-08-23 13:22:43 +0300

    Customize the display of Resource on the admin site

--- blog_link/admin.py
+++ blog_link/admin.py
@@ -5,6 +5,16 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Resource)
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('condition', 'created_on', 'tags')
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'condition', 'created_on', 'tag_list')
+    search_fields = ['title', 'content']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
 
 
