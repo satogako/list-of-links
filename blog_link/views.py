9949360ebbbed851e73ab511c610bb3d2ff7e3b8
Satogako <genevaq@ukr.net>
2023-08-24 15:05:51 +0300

    Add pagination to the views.py and create blank: HTML and css files

--- blog_link/views.py
+++ blog_link/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Resource
 
 # Create your views here.
+class ResourceList(generic.ListView):
+    """
+    Displays only those resources that have published. The results are 
+    sorted in reverse. The resource list is split into 8 items per page.
+    """
+    model = Resource
+    queryset = Resource.objects.filter(condition=1).order_by('-created_on')
+    paginate_by = 8
+    
