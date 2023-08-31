d52fab2be96f3971261c0155079fb11b349d3737
Satogako <genevaq@ukr.net>
2023-08-31 10:46:50 +0100

    Error when trying to add a comment page resourсe_details.html

--- blog_link/urls.py
+++ blog_link/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ResourceList.as_view(), name='all_resources'),
-    path('categories/', views.modal_view, name='categories')
+    path('categories/', views.modal_view, name='categories'),
+    path('<slug:slug>/', views.ResourceDetails.as_view(), name='resourсe_details'),
 ]
--- blog_link/views.py
+++ blog_link/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Resource
 
 # Create your views here.
@@ -16,3 +16,25 @@ class ResourceList(generic.ListView):
 
 def modal_view(request):
     return render(request, 'categories.html')
+
+class ResourceDetails(View):  
+    # ...
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Resource.objects.filter(condition=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        admired = False
+        if post.admirers.filter(id=self.request.user.id).exists():
+            admired = True
+
+        return render(
+            request,
+            "resourсe_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "admired": admired
+
+            },
+        )
+   
