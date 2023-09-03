3036a7f11d10fcb6c81f4b872de1eee62fed3876
Satogako <genevaq@ukr.net>
2023-09-03 17:08:51 +0100

    Add comments to the resource_details.html and index.html

--- blog_link/views.py
+++ blog_link/views.py
@@ -7,13 +7,22 @@ from taggit.models import Tag
 class ResourceList(generic.ListView):
     """
     Displays only those resources that have published. The results are 
-    sorted in reverse. The resource list is split into 8 items per page.
+    sorted in reverse. The resource list is split into 9 items per page.
     """
     model = Resource
     queryset = Resource.objects.filter(condition=1).order_by('-created_on')
     paginate_by = 9
     template_name = 'index.html'
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        resources = self.get_queryset()
+        comments = []
+        for resource in resources:
+            comments.append(resource.comments.filter(approved=True).order_by('created_on'))
+        context['comments'] = comments  
+        return context
+
 
 def modal_view(request):
     return render(request, 'categories.html')
